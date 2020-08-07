0e7ee5f275730fd7104c78367bcdd330631eb474
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-07 20:52:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1 
+    return false
+  
+  elsif
+    num == 2 || 3
+    return true
+  
+  else num > 1
+  
+  i = 2
+
+   num % i == 0 
+    return false
+    i += 1 
+  end
+end 
+
