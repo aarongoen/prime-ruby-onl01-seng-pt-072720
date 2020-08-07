a4c93f6421e0cf520846cba7f07a5d1d31d3af8b
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-07 21:08:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -18,7 +18,8 @@
 # end 
 
 def prime?(number)
-  if number <= 1
-    return false
-  # else (2..number-1).none? {|divisor| number % divisor == 0}
+  # if number <= 1
+  #   return false
+  # else 
+    (2..number-1).none? {|divisor| number % divisor == 0}
 end
