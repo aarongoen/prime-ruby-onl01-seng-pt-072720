89a820b67cbf424b55329f643cb4a4a6294cb7dc
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-07 20:57:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,40 @@
 # Add  code here!
-def prime?(num)
-  if num <= 1 
-    return false
+# def prime?(num)
+#   if num <= 1 
+#     return false
   
-  elsif
-    num == 2 || 3
-    return true
+#   elsif
+#     num == 2 || 3
+#     return true
   
-  else num > 1
+#   else num > 1
   
-  i = 2
+#   i = 2
 
-   num % i == 0 
-    return false
-    i += 1 
+#   num % i == 0 
+#     return false
+#     i += 1 
+#   end
+# end 
+
+
+is_prime = true
+2
+3
+number = item - 1
+4
+while number > 1
+5
+  if item % number != 0
+6
+    number = number - 1
+7
+  else
+8
+    is_prime = false
+9
+    break
+10
   end
-end 
-
+11
+end
