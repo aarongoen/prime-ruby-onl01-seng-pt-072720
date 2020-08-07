512ad889952ea38a6ccf732020b532066f301739
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-07 20:57:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -19,22 +19,12 @@
 
 
 is_prime = true
-2
-3
 number = item - 1
-4
 while number > 1
-5
   if item % number != 0
-6
     number = number - 1
-7
   else
-8
     is_prime = false
-9
     break
-10
   end
-11
 end
